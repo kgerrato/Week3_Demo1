db4c3d44e58501f5276298285db2efad091818aa
Konstantino Gerrato <kgerrato1@gmail.com>
2021-09-16 20:28:31 -0400

    Add Toast, Snackbar, two fields

---
+++ app/src/main/java/com/example/week3_demo1/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.week3_demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    EditText mEditText1, mEditText2;
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mEditText1 = (EditText) findViewById(R.id.et1);
+        mEditText1 = (EditText) findViewById(R.id.et2);
+
+        mTextView = (TextView) findViewById(R.id.tv1);
+    }
+
+    public void calculateSum(View view) {
+        mTextView.setText(""+(
+                Integer.parseInt(mEditText1.getText().toString()) +
+                        Integer.parseInt(mEditText1.getText())
+                )
+                );
+
+    }
+}
